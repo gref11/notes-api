8df416d6a445f5d4c64ea8dff4728a5014286df0
grefalb <grefalb11@gmail.com>
2025-04-15 21:55:52 +0300

    correct storage interface

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -6,7 +6,7 @@ import (
 
 type Storage interface {
 	GetAll() ([]models.Note, error)
-	GetByID() (*models.Note, error)
+	GetByID(id string) (*models.Note, error)
 	Create(note models.Note) error
 	Update(id string, note models.Note) error
 	Delete(id string) error
